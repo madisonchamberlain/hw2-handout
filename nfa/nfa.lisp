48e87267d8c7ca3b8773b8f055a41a8896be9add
mpchambe <mpchambe@ad3.ucdavis.edu@pc15.cs.ucdavis.edu>
2021-04-27 14:14:07 -0700

    better format; stilll 13/15

--- nfa/nfa.lisp
+++ nfa/nfa.lisp
@@ -1,30 +1,4 @@
-
-
-(defun checkStates (start final)
-  (cond
-    ; if one of the state options = the final option; return true
-    ((eq final start) t)
-
-    ; if not return null 
-    (t nil)
-  )
-)
-
-(defun nfa (transition start final input)
-  (cond
-    ; if no input; start must = final for true
-    ((null input) (car (mapcar #'(lambda (state) (checkStates state final)) start)))
-
-    (T nil
-      (setq states (funcall transition start (car input)))
-      (car (mapcar #'(lambda (state) (reachable transition (list state) final (cdr input))) states))
-    )
-  )
-)
-
 (defun reachable (transition start final input)
-  ;(print (list start))
-  ;(setq start (list start))
   (cond
     ; if no input; start must = final for true
     ((and (null input)  (equal start final))  T)
@@ -34,19 +8,9 @@
 
     ; otherwise pass to nfa function with start state in list form 
     (T
-
       ; get next states with funcall on the transition 
-      ;(print "start")
-      ;(print start)
-      ;(print input)
-      ;(print transition)
       (setq states (funcall transition start (car input)))
-      ;(print "next state options: ")
-      ;(print states)
-      (car (mapcar #'(lambda (state) (reachable transition state final (cdr input))) states))
-    
+      (car(mapcar #'(lambda (state) (reachable transition state final (cdr input))) states))
     )
   )
 )
-
-
