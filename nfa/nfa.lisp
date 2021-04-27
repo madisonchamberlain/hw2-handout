2f43fcc758e4f96d0f156a4a8ed29c7293078d63
mpchambe <mpchambe@ad3.ucdavis.edu@pc15.cs.ucdavis.edu>
2021-04-27 13:43:54 -0700

    passes 13/15 cases

--- nfa/nfa.lisp
+++ nfa/nfa.lisp
@@ -1,7 +1,52 @@
-; You may define helper functions here
+
+
+(defun checkStates (start final)
+  (cond
+    ; if one of the state options = the final option; return true
+    ((eq final start) t)
+
+    ; if not return null 
+    (t nil)
+  )
+)
+
+(defun nfa (transition start final input)
+  (cond
+    ; if no input; start must = final for true
+    ((null input) (car (mapcar #'(lambda (state) (checkStates state final)) start)))
+
+    (T nil
+      (setq states (funcall transition start (car input)))
+      (car (mapcar #'(lambda (state) (reachable transition (list state) final (cdr input))) states))
+    )
+  )
+)
 
 (defun reachable (transition start final input)
-    ;; TODO: Incomplete function
-    ;; The next line should not be in your solution.
-    (list 'incomplete)
+  ;(print (list start))
+  ;(setq start (list start))
+  (cond
+    ; if no input; start must = final for true
+    ((and (null input)  (equal start final))  T)
+
+    ; if no input and not equal return nil
+    ((and (null input)  (not (equal start final))) nil)
+
+    ; otherwise pass to nfa function with start state in list form 
+    (T
+
+      ; get next states with funcall on the transition 
+      ;(print "start")
+      ;(print start)
+      ;(print input)
+      ;(print transition)
+      (setq states (funcall transition start (car input)))
+      ;(print "next state options: ")
+      ;(print states)
+      (car (mapcar #'(lambda (state) (reachable transition state final (cdr input))) states))
+    
+    )
+  )
 )
+
+
