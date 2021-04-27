ca6aaae502f79837fc2ccef384108edf441b1c8f
mpchambe <mpchambe@ad3.ucdavis.edu@pc15.cs.ucdavis.edu>
2021-04-26 17:56:14 -0700

    works with 100% accuracy

--- match/match.lisp
+++ match/match.lisp
@@ -1,14 +1,16 @@
 ; add 1 for each element in list
 (defun len (list)
-    (cond
-        ; null list; return 0
-        ((null list) 0)
+  (cond
+    ; null list; return 0
+    ((null list) 0)
 
-        ; if list has 1 element return 1
-        ((null (cdr list)) 1)
+    ; if list has 1 element return 1
+    ((null (cdr list)) 1)
 
-        ; otherwise return 1 + length(rest of list)
-        (t (+ 1 (len(cdr list)))))) 
+    ; otherwise return 1 + length(rest of list)
+    (t (+ 1 (len(cdr list))))
+  )
+) 
 
 
 ; T if x is in list
@@ -26,48 +28,39 @@
 ) 
 
 ; return true if lists are equal excluding ?
-(defun questionMark (pattern assertion)
+(defun exclamationMark (pattern assertion)
   (cond
-    ; if both lists null; return true
-    ((and (null pattern) (null assertion)) T)
+    ; if the next item of each are the same proceed on those
+    ((equal (car (cdr pattern))  (car (cdr assertion)))  (patternMatch (cdr pattern) (cdr assertion)) )
 
-    ; if first element of pattern is ?, continue onto next elements
-    ((equal (car pattern) '?)  (questionMark (cdr pattern) (cdr assertion)))
+    ; if the next item of pattern is ?, proceed on both
+    ((equal (car (cdr pattern))  '?)  (patternMatch (cdr pattern) (cdr assertion)) )
 
-    ; if the first element of pattern = the first of assertion continue
-    ((equal (car pattern) (car assertion))  (questionMark (cdr pattern) (cdr assertion)))
+    ; if on the last element of assertion; proceed on cdr of both
+    ((null (cdr assertion)) (patternMatch (cdr pattern) (cdr assertion)))
 
-    ; otherwise it doesn't match, return false
-    (t nil)
+    ; otherwise continue on the same chunk of pattern, but progress on assertion
+    (t (patternMatch pattern (cdr assertion)))   
   )
 )
 
-; return true if lists are equal excluding ?
-(defun exclamationMark (pattern assertion)
+
+(defun patternMatch (pattern assertion)
   (cond
     ; if both lists null; return true
     ((and (null pattern) (null assertion)) T)
 
-    ; if the first element of pattern = the first of assertion continue
-    ((equal (car pattern) (car assertion))  (exclamationMark (cdr pattern) (cdr assertion)))
+    ; if nothing left in assertion but something left in pattern return nil
+    ((and (not (null pattern)) (null assertion)) nil)
 
+    ; if the next element of pattern = the next of assertion continue
+    ((equal (car pattern) (car assertion))  (patternMatch (cdr pattern) (cdr assertion)))
 
-    ; if first element of pattern is !
-    ((equal (car pattern) '!)  
-      (cond
-        ; if the next two elements are equal; continue to the next two
-        ((equal (car (cdr pattern)) (car (cdr assertion)))  (exclamationMark (cdr pattern) (cdr assertion)))
+    ; if next item of pattern is ! go to exmark
+    ((equal (car pattern) '!) (exclamationMark pattern assertion))
 
-        ; if on the last element of assertion; proceed on cdr of both
-        ((null (cdr assertion)) (exclamationMark (cdr pattern) (cdr assertion)))
-
-        ; otherwise continue on the same chunk of pattern, but progress on assertion
-        (t (exclamationMark pattern (cdr assertion)))   
-      )
-    )
-
-    ; if next item is ?
-
+    ; if next item is ?, continue with queston mark one
+    ((equal (car pattern) '?)  (patternMatch (cdr pattern) (cdr assertion)))
 
     ; otherwise it doesn't match, return false
     (t nil)
@@ -81,11 +74,8 @@
     ; if they are exact matches; return true
     ((equal pattern assertion) t)
 
-    ; if ! in pattern...
-    ((xInList '! pattern)  (exclamationMark pattern assertion))
-
-    ; if ? in pattern...
-    ((xInList '? pattern)  (questionMark pattern assertion))
+    ; if ! or ? in pattern see if they match with patternMatch
+    ((or (xInList '! pattern) (xInList '? pattern)) (patternMatch pattern assertion))
 
     ; otherwise they don't match
     (t nil)
